621866c7b168f938d78f64b498b1094ebd96e52d
h571560 <kristoffer-hp@hotmail.com>
2019-03-01 13:56:29 +0100

    la til workspace.xml i gitignore

--- project2/src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ project2/src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -1,8 +1,20 @@
 package no.hvl.dat110.messages;
 
 public class CreateTopicMsg extends Message {
-	
-	// TODO: 
-	// Implement objectvariables, constructor, get/set-methods, and toString method
-	
+
+    private String topic;
+
+
+    // TODO:
+    public CreateTopicMsg(MessageType msgType, String user, String topic ){
+        super(msgType,user);
+        this.topic = topic;
+    }
+    // Implement objectvariables, constructor, get/set-methods, and toString method
+
+
+    @Override
+    public String toString() {
+        return super.toString() +  " createTopicMsg[ + " + topic + "]";
+    }
 }
